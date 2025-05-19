9799d248d1843622ae5afa5e5c1e8f354d4597f9
yasarmustafa <160111036@ogr.gelisim.edu.tr>
2025-05-19 19:25:20 +0300

    Revoked Auth UI

--- app/page.tsx
+++ app/page.tsx
@@ -34,19 +34,10 @@ export default function App() {
   }
 
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
         <main>
-            <h1>Hello {user?.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-
           {/* StorageBrowser Component */}
           <h2>Your Files</h2>
           <StorageBrowser />
-
         </main>
-      )}
-    </Authenticator>
   );
 }
-
